34ed6445e682ee6d70142a4c7edd38e6e8f370cb
Adam Gordon <adambaumgordon@gmail.com>
2024-03-20 20:37:06 -0400

    fixing path function

--- logic/helper.py
+++ logic/helper.py
@@ -3,4 +3,4 @@ from pathlib import Path
 def getAbsPath():
     """Get the absolute path."""
     p = str(Path(__file__))
-    return p[:p.find("/letf-simulation")]+"/letf-simulation"
+    return p[:p.find("/letf-simulator")]+"/letf-simulator"
